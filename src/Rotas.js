1ce6be8c9de3a6c981e6536353275dc933c33421
majumxrs <mariaescola06@gmail.com>
2024-03-27 15:25:46 -0300

    tela de login completa e lincada com cadastro

--- src/Rotas.js
+++ src/Rotas.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
+
 import Home from "./Home";
 import Login from "./Login";
 import Agenda from "./Agenda";
